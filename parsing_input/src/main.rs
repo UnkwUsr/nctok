7f167e8facf7cb9d76895e3910185197bbc0925a
UnkwUsr <ktoto2707043@gmail.com>
2024-03-20 21:53:51 +0300

    parsing_input: refactor

--- parsing_input/src/main.rs
+++ parsing_input/src/main.rs
@@ -1,37 +1,38 @@
+use std::{collections::HashMap, io};
+
+type EntryPath = Vec<String>;
+
 #[derive(Debug)]
-enum HashEntry {
+enum Entry {
     Value(usize),
-    Children(HashMap<String, HashEntry>),
+    Children(HashMap<String, Entry>),
 }
 
-impl HashEntry {
+impl Entry {
     fn new_empty_children() -> Self {
-        HashEntry::Children(HashMap::<String, HashEntry>::new())
+        Entry::Children(HashMap::<String, Entry>::new())
     }
 
-    fn add_nested_items(&mut self, path: Vec<String>, name: String, value: usize) {
+    fn add(&mut self, path: EntryPath, name: String, value: usize) {
         match self {
-            HashEntry::Value(_) => (),
-            HashEntry::Children(childs) => {
+            Entry::Value(_) => (),
+            Entry::Children(childs) => {
                 if path.is_empty() {
-                    childs.insert(name, HashEntry::Value(value));
+                    childs.insert(name, Entry::Value(value));
                     return;
                 }
 
                 let key = path[0].clone();
-                let child = childs.entry(key).or_insert(HashEntry::new_empty_children());
+                let child = childs.entry(key).or_insert(Entry::new_empty_children());
 
-                child.add_nested_items(path[1..].to_vec(), name, value);
+                child.add(path[1..].to_vec(), name, value);
             }
         }
     }
 }
 
-use std::{collections::HashMap, io};
-
 fn main() {
-    type ParentPath = Vec<String>;
-    let mut root = HashEntry::new_empty_children();
+    let mut root = Entry::new_empty_children();
 
     io::stdin().lines().for_each(|x| {
         let binding = x.unwrap();
@@ -39,9 +40,9 @@ fn main() {
 
         let number: usize = y.next().unwrap().parse().unwrap();
         let name = y.next_back().unwrap().to_string();
-        let parent_path: ParentPath = y.map(str::to_string).collect();
+        let parent_path: EntryPath = y.map(str::to_string).collect();
 
-        root.add_nested_items(parent_path, name, number);
+        root.add(parent_path, name, number);
     });
 
     println!("{:#?}", root);
